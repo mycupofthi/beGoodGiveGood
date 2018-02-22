03ce93d5ec6a67d382b4df15e2c90c2cacbf3495
Thi Tran <thitran91@gmail.com>
2018-02-22 17:09:58 -0500

    Initial commit

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -5,10 +5,12 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      people = [],
-      
-    }
+      name: '',
+      birthday: '',
+      interests: ''
+    };
   }
+  handleChange
     render() {
       return (
         <div>
@@ -16,11 +18,11 @@ class App extends React.Component {
           {/* create a form that will take the name, interests and birthday of the person */}
           <form action="">
             <label htmlFor="name">Name:</label>
-            <input type="text" value="Enter name" onChange="" id="name" />
+            <input type="text" value="Enter name" onChange="handleChange" id="name" />
             <label htmlFor="birthday">Birthday:</label>
-            <input type="text" value="Enter Birthday" id="birthday" />
+            <input type="text" value="mm-dd" id="birthday" onChange="handleChange" />
             <label htmlFor="interests">Interests:</label>
-            <input type="text" value="Enter interests" onChange="" id="interests" />
+            <input type="text" value="Enter interests" onChange="handleChange" id="interests" />
             <input type="submit" value="Add Person" />
           </form>
         </div>
